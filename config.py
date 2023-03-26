f905ce187ac023a7601b083d232be8d9b07a5c3e
Hyper X Rachit <122621466+Hyper-x-Rachit@users.noreply.github.com>
2023-03-26 10:03:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ load_dotenv()
 class Config(object):
     LOGGER = True
 
-    API_ID = int(getenv("API_ID", 6))
-    API_HASH = getenv("API_HASH", None)
+    API_ID = int(getenv("API_ID", "15469484"))
+    API_HASH = getenv("API_HASH", "a4e47ac121ccd896f52fc815a9a10a8e")
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
     STRING_SESSION = getenv("STRING_SESSION", None)
     SPAMWATCH_API = None
-    TOKEN = getenv("TOKEN", None)
-    OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
-    MONGO_URI = getenv("MONGO_DB_URI")
+    TOKEN = getenv("TOKEN", "5980016986:AAG1mUJbgvpwNKMFdEtgRFGBAaWUGAAOiLg")
+    OWNER_ID = int(getenv("OWNER_ID", "5824231649"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "HYPER_X_RACHIT")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Shona_AI")
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001962386072"))
+    MONGO_URI = getenv("MONGO_DB_URI", "mongodb+srv://Rachit:Rachit@cluster0.hzww9ra.mongodb.net/?retryWrites=true&w=majority")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
     DATABASE_URL = getenv("DATABASE_URL")
 
